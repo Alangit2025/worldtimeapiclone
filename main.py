a892fe2620117c0060795a3a20d04d9d60dd2d40
Alangit2025 <alncaballero@gmail.com>
2025-05-11 14:38:31 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"mensaje": "API funcionando correctamente"}
+
+@app.get("/api/time/{timezone}")
+def get_time(timezone: str):
+    try:
+        tz = ZoneInfo(timezone)
+        now = datetime.now(tz)
+        return {
+            "timezone": timezone,
+            "datetime": now.isoformat(),
+            "utc_offset_hours": now.utcoffset().total_seconds() / 3600,
+            "day_of_week": now.strftime("%A"),
+            "day_of_year": now.timetuple().tm_yday,
+            "unixtime": int(now.timestamp())
+        }
+    except Exception:
+        return {"detail": "Zona horaria no válida"}
